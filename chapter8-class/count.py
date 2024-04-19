c67e74a963d95f22e739c841966980ba61d6862c
sponbob-pat <ixdos1111@gmail.com>
2024-04-19 15:02:20 +0900

    chapter8 완료

--- chapter8-class/count.py
+++ chapter8-class/count.py
@@ -1,11 +1,22 @@
 
 class CountFromBy : 
 
-    def __init__(self, v : int, i : int) -> None : 
+    # v, i의 기본값을 0과 1로 설정
+    def __init__(self, v : int = 0, i : int = 1) -> None : 
         
         self.val = v
         self.incr = i
+        
+    def __repr__(self) -> str : 
+        return str(self.val)
 
     def increase(self) -> None : 
         self.val += self.incr
 
+if __name__ == '__main__' : 
+    
+    countFromBy = CountFromBy(123, 4444)
+    
+    print(countFromBy) # __repr__를 이용해서
+                       # str(self.val)을 반환하기 때문에
+                       # 123을 출력한다. 
